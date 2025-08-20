415f93ae72f75339cae7393e1805977d2f79a5af
GetPrize9321 <bruceaireland@hotmail.com>
2025-08-20 12:08:50 +0800

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,4 @@
 function changeBackgroundColor() {
-    const colors = ["0000ff", "00ff00", "ff0000", "000000"];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
+    const colours = ["0000ff", "00ff00", "ff0000", "000000"];
+    document.body.style.backgroundColor = colours;
 }
